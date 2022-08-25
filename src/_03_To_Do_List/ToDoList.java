982fe73bd1d60a38d1aa1147391e5898f91f0489
league <league@imac-18.attlocal.net>
2022-08-24 18:28:42 -0700

    almost

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,43 +1,53 @@
 package _03_To_Do_List;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ToDoList {
+public class ToDoList implements ActionListener {
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton add = new JButton("ADD Task");
+	JButton remove = new JButton("REMOVE Task");
+	JButton view = new JButton("VIEW Tasks");
+	JButton save = new JButton("SAVE Tasks");
+	JButton load = new JButton("LOAD Tasks");
+	ArrayList<String> tasks = new ArrayList<String>();
+
 	/*
-	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
+	 * Create a program with five buttons, add task, view tasks, remove task, save
+	 * list, and load list.
 	 *
-	 * When add task is clicked:
-	 * 		Create a JOptionPane to ask the user for a task and add it to an ArrayList
+	 * When add task is clicked: Create a JOptionPane to ask the user for a task and
+	 * add it to an ArrayList
 	 * 
-	 * When the view tasks button is clicked:
-	 *		show all the tasks in the list
+	 * When the view tasks button is clicked: show all the tasks in the list
 	 * 
-	 * When the remove task button is clicked:
-	 * 		Create a JOptionPane to prompt the user for which task to remove and take it off of the list.
+	 * When the remove task button is clicked: Create a JOptionPane to prompt the
+	 * user for which task to remove and take it off of the list.
 	 * 
-	 * When the save list button is clicked:
-	 * 		Save the list to a file
+	 * When the save list button is clicked: Save the list to a file
 	 * 
-	 * When the load list button is clicked:
-	 * 		Create a JOptionPane to Prompt the user for the location of the file and load the list from that file
+	 * When the load list button is clicked: Create a JOptionPane to Prompt the user
+	 * for the location of the file and load the list from that file
 	 * 
-	 * When the program starts, it should automatically load the last saved file into the list. 
+	 * When the program starts, it should automatically load the last saved file
+	 * into the list.
 	 */
-	public static void main(String[] args) {
-		JFrame frame= new JFrame();
-		JPanel panel= new JPanel();
-		JButton add = new JButton("ADD Task");
-		JButton remove = new JButton("REMOVE Task");
-		JButton view = new JButton("VIEW Tasks");
-		JButton save = new JButton("SAVE Tasks");
-		JButton load = new JButton("LOAD Tasks");
-		add.addActionListener(null);
-		remove.addActionListener(null);
-		view.addActionListener(null);
-		save.addActionListener(null);
-		load.addActionListener(null);
+	void run() {
+
+		add.addActionListener(this);
+		remove.addActionListener(this);
+		view.addActionListener(this);
+		save.addActionListener(this);
+		load.addActionListener(this);
 		panel.add(add);
 		panel.add(remove);
 		panel.add(save);
@@ -46,9 +56,51 @@ public class ToDoList {
 		frame.add(panel);
 		frame.pack();
 		frame.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new ToDoList().run();
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if (e.getSource() == add) {
+			String newTask = JOptionPane.showInputDialog("What is your new task");
+			tasks.add(newTask);
+			JOptionPane.showMessageDialog(null, newTask+" added");
+		}
+		else if(e.getSource()== view) {
+			String complete="";
+			for(int i=0; i<tasks.size();i++) {
+				complete=complete+(i+1)+": "+tasks.get(i)+"\n";
+			}
+			JOptionPane.showMessageDialog(null, complete);
+		}
+		else if(e.getSource()== remove) {
+			String complete="";
+			for(int i=0; i<tasks.size();i++) {
+				complete=complete+(i+1)+": "+tasks.get(i)+"\n";
+			}
+			String choose= JOptionPane.showInputDialog("which task do you want to remove,\n"+ complete+"\n give number");
+			int num= Integer.parseInt(choose);
+			tasks.remove(num-1);
+		}
+		else if(e.getSource()==save) {
+			String complete="";
+			for(int i=0; i<tasks.size();i++) {
+				complete=complete+(i+1)+": "+tasks.get(i)+"\n";
+			}
+			try { FileWriter fw = new FileWriter("src/_03_To_Do_List/toDoList.txt") ;
+			fw.write(complete);
+			fw.close();
+			}
+			catch (IOException f) {
+				// TODO Auto-generated catch block
+				f.printStackTrace();
+			}
+		}
 		
-
-
-
 	}
 }
